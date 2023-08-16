5c09f334ba2067c7a0db23a7780b197f655beab9
ricardomadeira93 <ricardomadeira1993@gmail.com>
2023-08-16 19:22:42 +0100

    .

---
+++ client.ts
@@ -0,0 +1,9 @@
+import { QdrantClient } from '@qdrant/js-client-rest';
+
+const client = new QdrantClient({
+  url: 'https://cc5123d8-850c-417f-a32c-7fb7233e8202.eu-central-1-0.aws.cloud.qdrant.io:6333',
+  apiKey:
+    'gv3-m15AWTFF1cpt48WQndapzV5bAoheO6vxqlYIS-xtaYfw0VwEoA',
+});
+
+    
---
+++ config.ts
@@ -0,0 +1,2 @@
+export const indexName = 'langchain-tutorial'
+export const timeout = 180000
